3314e80459d47917a851534e9551449f82c3d9a6
Victor Manuel Lajara Andreu <victormlajara03@gmail.com>
2021-09-21 18:41:07 +0200

    ejer 4 bien2¿?

--- public/Ejercicio4/insert.php
+++ public/Ejercicio4/insert.php
@@ -2,6 +2,6 @@
     function insert($tabla, $array){
         $sql_sentecia = "insert into '%s' ('%s') values('%s') ";
         $nombre = implode(",",array_key($array));
-        $valores = implode(",",$array);
+        $valores = implode("\",\"",$array);
         echo sprintf($sql_sentecia,$tabla,$nombre,$valores);
     }
