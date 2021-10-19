9d0882b77166ff1e65a2850c45a75017a833bc79
Victor Manuel Lajara Andreu <victormlajara03@gmail.com>
2021-10-19 12:02:22 +0200

    bsae datos 2

--- public/Ejercicio4-1/formulario.php
+++ public/Ejercicio4-1/formulario.php
@@ -7,17 +7,21 @@ use App\Alumne;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     if($_POST['nombre'] === "" || $_POST["birthday"] === date("Y-m-d") || $_POST["birthday"] === ""
-        || empty($_POST["genero"])  || $_FILES['imagen']['type'] !== 'image/jpeg' )
+        || empty($_POST["genero"])   )
     {
         echo "Formulario Incompleto <br>";
         echo "<a href='formulario.php'> volver</a>";
     } else{
-
         $name = basename($_FILES['imagen']['name']);
+       if ($_FILES['imagen']['type'] !== 'image/jpeg'){
+           $name = null;
+       }
         move_uploaded_file($_FILES['imagen']['tmp_name'],"imagen/$name" );
         $newAlumno = new Alumne($_POST['dni'],$_POST['nombre'],$_POST['birthday'],$_POST['genero'],$_POST['hobbies'],$name);
         $query->insertAlumne('alumnes',$newAlumno);
-        require('mostrarTabla.php');
+        echo "Alumno añadido";
+        echo "<a href='mostrarTabla.php'> Ver tablas</a>";
+
     }
 } else {
     require('formulario.view.php');
